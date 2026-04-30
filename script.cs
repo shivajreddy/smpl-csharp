8bdc60db9c24b7ecf671ec6c85e678bd1dfc3c38
smpl <sreddy@hey.com>
2026-04-30 11:57:04 -0700

    .

--- script.cs
+++ script.cs
@@ -1,27 +1,11 @@
-using System.Globalization;
-
 namespace Script;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("hello there");
-        var p = new Person { Name = "smpl", age = 30 };
-        p.Greet();
+        Console.WriteLine("Hello, World!");
     }
 }
 
-class Person
-{
-    public string? Name { get; set; }
-    public int age;
-
-    public void Greet()
-    {
-        var culture = CultureInfo.CurrentCulture;
-        var greeting = $"Hello, {Name}! Culture: {culture.Name}";
-        Console.WriteLine(greeting);
-    }
-}
 
