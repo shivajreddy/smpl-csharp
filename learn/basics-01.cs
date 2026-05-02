9b7e4679cbc52f5dc74b8e024cb7996e979781b9
smpl <sreddy@hey.com>
2026-05-01 19:45:12 -0700

    .

--- learn/basics-01.cs
+++ learn/basics-01.cs
@@ -4,8 +4,20 @@ class Program
 {
     static void Main()
     {
-        // Console.WriteLine("wo");
+        var p1 = new PersonRecord("smpl", 30);
+        Console.WriteLine(p1);
+
+        // var p2 = new PersonClass() { Name = "smpl", Age = 30 };
+        // var p3 = new PersonClass();
+        var p4 = new PersonClass("smpl", 30);
+        Console.WriteLine(p4);
     }
 }
 
-public record class Person(string Name, int Age);
+public record class PersonRecord(string Name, int Age);
+public class PersonClass
+{
+    public string Name;
+    public int Age;
+    public PersonClass(string name, int age) { Name = name; Age = age; }
+}
